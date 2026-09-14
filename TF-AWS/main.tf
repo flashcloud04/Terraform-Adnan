d533ecdc3d9909f6b47fa439d5ef4c42f2e54dbe
Adnan Ahmed Banikar <flashcloud30@gmail.com>
2026-09-14 23:16:53 +0530

    This commit updated the main.tf

--- TF-AWS/main.tf
+++ TF-AWS/main.tf
@@ -6,23 +6,30 @@ terraform {
     }
   }
 }
-resource "aws_vpc" "name" {
-  cidr_block = "10.0.0.0/16"
+
+# Use the already-created VPC
+data "aws_vpc" "existing" {
+  id = "vpc-098884ed175c0a912"
+}
+
+# Create a subnet inside the existing VPC
+resource "aws_subnet" "public" {
+  vpc_id     = data.aws_vpc.existing.id
+  cidr_block = "10.0.2.0/24"
+
   tags = {
-    Name = "my-vpc"
+    Name = "my-public-subnet"
   }
 }
+
+# Create EC2 instance inside the new subnet
 resource "aws_instance" "myWebServer" {
   ami           = "ami-0354c98ae10b02961"
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public.id
+
+  subnet_id = aws_subnet.public.id
 
   tags = {
     Name = "MyWebServerVPC"
   }
-
-}
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.name.id
-  cidr_block = "10.0.1.0/24"
 }
