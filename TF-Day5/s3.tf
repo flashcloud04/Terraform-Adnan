db50f8c05d61137bba56a4a0504d2cb2a677064b
Adnan Ahmed Banikar <flashcloud30@gmail.com>
2026-09-18 20:49:04 +0530

    added s3 backend resource and saved state file in s3

--- TF-Day5/s3.tf
+++ TF-Day5/s3.tf
@@ -3,9 +3,17 @@
 # ============================================================
 
 resource "aws_s3_bucket" "project" {
-  bucket_prefix = "${local.name_prefix}-artifacts-"
+  bucket = "cloud-platform-dev-artifacts-f3011686ca55be26854f0c1a54"
 
   tags = {
     Name = "${local.name_prefix}-artifacts"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "project" {
+  bucket = aws_s3_bucket.project.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
--- TF-Day5/versions.tf
+++ TF-Day5/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.10.0"
 
+  backend "s3" {
+    bucket       = "cloud-platform-dev-artifacts-f3011686ca55be26854f0c1a54"
+    key          = "environments/dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
