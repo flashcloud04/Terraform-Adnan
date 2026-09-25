08c469c27d199b7b2a46e1d3482e5600b8a605e4
Adnan Ahmed Banikar <flashcloud30@gmail.com>
2026-09-26 00:14:21 +0530

    vpc creation

---
+++ TF-Day9/vpc/subnets.tf
@@ -0,0 +1,51 @@
+# -------------------------
+# Public Subnets
+# -------------------------
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public-${count.index + 1}"
+    Tier = "public"
+  }
+}
+
+# -------------------------
+# Private Application Subnets
+# -------------------------
+
+resource "aws_subnet" "private_app" {
+  count = length(var.private_app_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_app_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private-app-${count.index + 1}"
+    Tier = "private-app"
+  }
+}
+
+# -------------------------
+# Private Database Subnets
+# -------------------------
+
+resource "aws_subnet" "private_db" {
+  count = length(var.private_db_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_db_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private-db-${count.index + 1}"
+    Tier = "private-db"
+  }
+}
